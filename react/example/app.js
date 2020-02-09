a789ed038237fa3c5ec70c7841c024805cacbc44
Aliaksei_Petrachenka <dr.deser@tut.by>
2020-02-09 16:28:47 +0300

    init

---
+++ react/example/app.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import MaskedInput from '../src/reactTextMask'
+
+export default () => (
+  <form className='form-horizontal'>
+    <div className='form-group'>
+      <label htmlFor='1' className='col-sm-2 control-label'>Masked input</label>
+
+      <div className='col-sm-10'>
+        <MaskedInput
+          mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+          className='form-control'
+          id='1'
+          type='text'
+		  keepCharPositions
+		  showMask
+		  placeholderChar="_"
+        />
+      </div>
+    </div>
+  </form>
+)
